ae95a01cc28e538d03bef076eb0b51511893a1c8
Michaela Havlíková <Michelle.h@seznam.cz>
2024-11-28 11:30:00 +0100

    Optimize img using NuxtImg with desities and format

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -4,7 +4,7 @@ N<script setup lang="ts"></script>
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" class="flex gap-1 items-center">
-        <img class="w-14" src="/icon-green.png" alt="">
+        <NuxtImg width="56px" densities="x1" src="/icon-green.png" format="webp" alt="" />
         <span class="text-3xl font-bold">NRecipes</span>
       </NuxtLink>
       <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
