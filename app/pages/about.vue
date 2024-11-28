fdac8db605ecfbc0b2b14eaafa37161c43cf61e1
Michaela Havlíková <Michelle.h@seznam.cz>
2024-11-28 11:09:40 +0100

    Create BaseNavigation component and about page, use NuxtLink

---
+++ app/pages/about.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <main>
+    <section class="container py-20">
+      <h1 class="text-6xl font-extrabold mb-6 text-balance">About NRecipes:</h1>
+      <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">NRecipes is a recipe application based on the dummyJSON API, created to <span class="font-bold">build modern app with Nuxt</span>.</p> 
+    </section>
+  </main>
+</template>
