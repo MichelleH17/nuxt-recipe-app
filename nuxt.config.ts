2f42921d10d9b42022a9acfe871a708def221222
Michaela Havlíková <Michelle.h@seznam.cz>
2024-12-04 12:35:31 +0100

    Add nuxt ui module and delete tailwindcss and icon modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,10 +34,9 @@ export default defineNuxtConfig({
   },
 
   modules: [
-    '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
+    '@nuxt/image',
+    '@nuxt/ui'
   ],
 
   googleFonts: {
