6549abf3d1b1a86fdb455e3f781ee9e7430d2cb3
Michaela Havlíková <Michelle.h@seznam.cz>
2024-12-05 11:23:14 +0100

    Replace Icon and Button with UIcon and UButton

--- app/components/RecipeCard.vue
+++ app/components/RecipeCard.vue
@@ -19,19 +19,24 @@ defineProps<{
       <p class="text-xl lg:text-2xl font-semibold mb-2">{{ recipe.name }}</p>
       <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
         <div class="flex items-center gap-1">
-          <Icon name="mdi:clock-time-eight-outline" style="color: #f79f1a" />
+          <UIcon name="i-mdi-clock-time-eight-outline" class="text-dodgeroll-gold-400" />
           <span>{{ recipe.cookTimeMinutes }}</span>
         </div>
         <div class="flex items-center gap-1">
-          <Icon name="mdi:fire" style="color: #f79f1a" />
+          <UIcon name="i-mdi-fire" class="text-dodgeroll-gold-400" />
           <span>{{ recipe.caloriesPerServing }}</span>
         </div>
         <div class="flex items-center gap-1">
-          <Icon name="mdi:star" style="color: #f79f1a" />
+          <UIcon name="i-mdi-star" class="text-dodgeroll-gold-400" />
           <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
         </div>
       </div>
-      <BaseBtn :to="`/recipes/${recipe.id}`" label="View" />
+      <UButton 
+        :ui="{
+          base: 'self-start',
+        }"
+        :to="`/recipes/${recipe.id}`" 
+        label="View" />
     </div>
   </div>
 </template>
