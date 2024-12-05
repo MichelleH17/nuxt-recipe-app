115d122ee46ff73d5ff17b79ac813d66af2f32b5
Michaela Havlíková <Michelle.h@seznam.cz>
2024-12-05 08:28:14 +0100

    Create file to configure nuxt ui

---
+++ app/app.config.ts
@@ -0,0 +1,16 @@
+export default defineAppConfig({  
+  ui: {
+    primary: "dodgeroll-gold",
+    gray: 'cool',
+    button: {
+      default: {
+        size: "lg",
+        color: "blue"
+      },
+      rounded: "rounded-full",
+      size: {
+        lg: "text-xl"
+      }
+    },
+  }
+})
